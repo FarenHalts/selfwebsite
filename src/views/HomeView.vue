01503268cdf4b23f855124a0c02bc151af96dac0
Lucas Garcia <lucas.garcia_021@hotmail.com>
2022-06-09 22:58:07 -0300

    First Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <input type="number" v-model="inputData">
+    <div v-if="inputData">{{squareRoot}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  data(){
+    return{
+      inputData: ''
+    }
+  },
+  computed: {
+    squareRoot(){
+      return Math.sqrt(this.inputData)
+    }
+  }
+}
+</script>
